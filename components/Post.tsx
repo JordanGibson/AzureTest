ea7e1f386466a12a65d671daf7db7f6d49350461
Jordan Gibson <jordang@kainos.com>
2022-06-20 21:21:38 +0100

    Add like and comment icons

--- components/Post.tsx
+++ components/Post.tsx
@@ -8,6 +8,8 @@ import {
   Spacer,
   Text,
 } from "@nextui-org/react";
+import { BsChat, BsHeart } from "react-icons/bs";
+import { FaHeart, FaRegComment, FaRegHeart } from "react-icons/fa";
 
 export default function Post() {
   return (
@@ -19,7 +21,17 @@ export default function Post() {
           <Text>{faker.internet.userName()}</Text>
         </Row>
         <Spacer y={0.5}></Spacer>
-        <Image src={faker.image.image(800, 800, true)}></Image>
+        <Image
+          className="rounded"
+          src={faker.image.image(800, 800, true)}
+        ></Image>
+        <Spacer y={0.8}></Spacer>
+        <Row className="text-gray-700 outline-gray-700" gap={0.7}>
+          <FaRegHeart size={"1.8em"} />
+          <Text>{faker.datatype.number()}</Text>
+          <FaRegComment size={"1.8em"} />
+          <Text>{faker.datatype.number()}</Text>
+        </Row>
       </Card.Body>
     </Card>
   );
